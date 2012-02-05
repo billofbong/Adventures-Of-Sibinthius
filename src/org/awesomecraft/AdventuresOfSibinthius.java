e9936485a3f84fa78523d04f065b8bc121a942a7
Will Wright <willjwright@mac.com>
2012-02-05 11:46:53 -0800

    Added saving and loading! Hooray!

--- src/org/awesomecraft/AdventuresOfSibinthius.java
+++ src/org/awesomecraft/AdventuresOfSibinthius.java
@@ -13,7 +13,8 @@ import javax.swing.JLabel;
  * @author Will Wright
  */
 public class AdventuresOfSibinthius extends JFrame {
-    private boolean shouldLoop = true;
+    private boolean loop1 = true;
+    private boolean loop2 = true;
     private Random gen = new Random();
     private int fight = gen.nextInt(1);
     private int sneak = gen.nextInt(1);
@@ -28,8 +29,12 @@ public class AdventuresOfSibinthius extends JFrame {
     JLabel nar1 = new JLabel("You wake up in a dungeon.");
     JLabel nar2 = new JLabel("What do you do?");
     public AdventuresOfSibinthius() {
-        forward.setVisible(false);
-        back.setVisible(false);
+        try {
+        Player.loc = Double.valueOf(SaveLoad.load("/Users/Will/Sibinthius.bin").toString());
+        System.out.println("Loaded game at location " + Player.loc + ".");
+        } catch(Exception ex) {
+            System.err.println("Could not load Save: Creating a new one.");
+        }
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(new FlowLayout());
         add(nar1);
@@ -40,8 +45,10 @@ public class AdventuresOfSibinthius extends JFrame {
         add(back);
         setSize(205, 170);
         setVisible(true);
-        while(shouldLoop) {
+        while(true) {
            if(Player.loc == 1) {
+           forward.setVisible(false);
+           back.setVisible(false);
            left.setText("Do nothing");
            left.addActionListener(AList.q1l);
            right.setText("Pick the lock");
@@ -51,6 +58,15 @@ public class AdventuresOfSibinthius extends JFrame {
            } if(Player.loc == 2) {
                    nar1.setText("You are now outside");
                    nar2.setText("the door. What now?");
+                   if(loop1) {
+                   try {
+                   SaveLoad.save(Player.loc, "/Users/Will/Sibinthius.bin");
+                   System.out.println("Saved game at location " + Player.loc + ".");
+                   } catch(Exception ex) {
+                       System.err.println("Failed to save game");
+                   }
+                   loop1 = false;
+                   }
                    left.removeActionListener(AList.q1l);
                    right.removeActionListener(AList.q1r);
                    forward.addActionListener(AList.q2g);
@@ -62,6 +78,15 @@ public class AdventuresOfSibinthius extends JFrame {
                    left.setText(lft);
                    forward.setVisible(true);
            }   if(Player.loc == 3) {
+                   if(!loop1) {
+                   try {
+                   SaveLoad.save(Player.loc, "/Users/Will/Sibinthius.bin");
+                   System.out.println("Saved game at location " + Player.loc + ".");
+                   } catch(Exception ex) {
+                       System.err.println("Failed to save game");
+                   }
+                   loop1 = true;
+                   }
                forward.setVisible(false);
                nar1.setText("You hear a guard.");
                nar2.setText("What do you do now?");
